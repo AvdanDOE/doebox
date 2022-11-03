e6ba3db624e2a2601e91a6ec10c4d9926717915d
Keri <kerichuudev@gmail.com>
2022-11-03 15:49:06 +0400

    reformat

--- configuration/eww/dock/scripts/grabvolume.sh
+++ configuration/eww/dock/scripts/grabvolume.sh
@@ -4,8 +4,6 @@ ICONSET=("󰕿" "󰖀" "󰕾")
 VOLUME=`pamixer --get-volume`
 INDEX=$((VOLUME * 3 / 100))
 
-if [ "$INDEX" -ge 3 ]; then
-	INDEX=2
-fi
+if [ "$INDEX" -ge 3 ]; then INDEX=2; fi
 
 echo ${ICONSET[$INDEX]}
