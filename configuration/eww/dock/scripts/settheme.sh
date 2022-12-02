640a1f7ce6e0b20a5235c37f027b440534a7208b
kerichdev <kerichuudev@gmail.com>
2022-12-02 18:50:30 +0300

    add proper themeing from v1lune

--- configuration/eww/dock/scripts/settheme.sh
+++ configuration/eww/dock/scripts/settheme.sh
@@ -1,7 +1,8 @@
-if [[ `cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name | cut -d "=" -f 2` == "Cutefish-dark" && `grep '$bgc: #fff;' ./eww.scss` == '$bgc: #fff;' ]]
-    then sed -i 's/$bgc: #fff;/$bgc: #000;/' ./eww.scss; sed -i 's/$fgc: #000;/$fgc: #fff;/' ./eww.scss
-fi
+#!/usr/bin/env bash
 
-if [[ `cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name | cut -d "=" -f 2` == "Cutefish-light" && `grep '$bgc: #000;' ./eww.scss` == '$bgc: #000;' ]]
-    then sed -i 's/$bgc: #000;/$bgc: #fff;/' ./eww.scss; sed -i 's/$fgc: #fff;/$fgc: #000;/' ./eww.scss
+if [[ `cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name | cut -d "=" -f 2` == "Cutefish-dark" && `grep '$bgc: #fff;' ./eww.scss` == '$bgc: #fff;' ]]
+    then sed -i 's/fill="black"/fill="white"/g' ../globalassets/avlogo.svg && sed -i 's/$bgc: #fff;/$bgc: #000;/' ./eww.scss; sed -i 's/$fgc: #000;/$fgc: #fff;/' ./eww.scss
+
+elif [[ `cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name | cut -d "=" -f 2` == "Cutefish-light" && `grep '$bgc: #000;' ./eww.scss` == '$bgc: #000;' ]]
+    then sed -i 's/fill="white"/fill="black"/g' ../globalassets/avlogo.svg && sed -i 's/$bgc: #000;/$bgc: #fff;/' ./eww.scss; sed -i 's/$fgc: #fff;/$fgc: #000;/' ./eww.scss
 fi
