050c28ab275f256ee4d4d24ec31940bcf6ea021b
kerichdev <kerichuudev@gmail.com>
2022-12-02 18:49:04 +0300

    clear unused scripts

--- configuration/eww/dock/scripts/grabfreespace.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-case $1 in
-    pcusd) df --output=pcent / | tail -n 1 | xargs ;;
-    gsize) df --output=size / -h | tail -n 1 | xargs ;;
-    gusd) df --output=used / -h | tail -n 1 | xargs ;;
-esac
--- configuration/eww/dock/scripts/themewatcher.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-tail -F ~/.config/cutefishos/theme.conf 2> /dev/null | sed -n 's/.*AccentColor=\([0-9]*\).*/\1/p'
