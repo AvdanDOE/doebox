077bcbb5f976dfbbf61216c84aaab8ce64cca4a8
kerichdev <kerichuudev@gmail.com>
2022-12-02 18:49:19 +0300

    add the volume script from v1lune

--- configuration/eww/dock/scripts/playground.sh
+++ configuration/eww/dock/scripts/playground.sh
@@ -15,3 +15,16 @@ map() {
 LEVEL=`map $(pamixer --get-volume) 1 100 1 3`
 
 echo ${ICONSET[$LEVEL]}
+
+
+#!/usr/bin/env bash
+
+ICONSET=("󰕿" "󰖀" "󰕾")
+VOLUME=`pamixer --get-volume`
+INDEX=$((VOLUME * 3 / 100))
+
+if [ "$INDEX" -ge 3 ]; then
+	INDEX=2
+fi
+
+echo ${ICONSET[$INDEX]}
