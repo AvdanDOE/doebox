e58f2ce23f6f907a96a9c80bbe54138cb0bbef38
kerichdev <kerichuudev@gmail.com>
2023-01-16 18:02:15 +0300

    fix second background color

--- configuration/eww/dock/scripts/settheme.sh
+++ configuration/eww/dock/scripts/settheme.sh
@@ -5,7 +5,7 @@ if [[ `cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name | cut -d "=" -f 
     sed -i 's/fill="#1c1c1d"/fill="#ffffff"/g' ../globalassets/avlogo.svg
     sed -i 's/$bgc: #fff;/$bgc: #1c1c1d;/' ./eww.scss
     sed -i 's/$fgc: #1c1c1d;/$fgc: #fff;/' ./eww.scss
-    sed -i 's/$sbg: #fff;/$sbg: #2c2c2d;/' ./eww.scss
+    sed -i 's/$sbg: #f3f4f9;/$sbg: #2c2c2d;/' ./eww.scss
     sed -i 's/$lacct;/$dacct;/g' ./eww.scss
 
 elif [[ `cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name | cut -d "=" -f 2` == "Cutefish-light" && `grep '$bgc: #1c1c1d;' ./eww.scss` == '$bgc: #1c1c1d;' ]]
@@ -13,6 +13,6 @@ elif [[ `cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name | cut -d "=" -
     sed -i 's/fill="#ffffff"/fill="#1c1c1d"/g' ../globalassets/avlogo.svg
     sed -i 's/$bgc: #1c1c1d;/$bgc: #fff;/' ./eww.scss
     sed -i 's/$fgc: #fff;/$fgc: #1c1c1d;/' ./eww.scss
-    sed -i 's/$sbg: #2c2c2d;/$sbg: #fff;/' ./eww.scss
+    sed -i 's/$sbg: #2c2c2d;/$sbg: #f3f4f9;/' ./eww.scss
     sed -i 's/$dacct;/$lacct;/g' ./eww.scss
 fi
