8752ee4c7d6b9bb3760ed17aa69c0361b980b1f3
kerichdev <kerichuudev@gmail.com>
2023-03-12 21:11:37 +0300

    upstream main to v1lune-dp2

--- configuration/eww/dock/scripts/settheme.sh
+++ configuration/eww/dock/scripts/settheme.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
 if [[ `cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name | cut -d "=" -f 2` == "Cutefish-dark" && `grep '$bgc: #fff;' ./eww.scss` == '$bgc: #fff;' ]]
-    then sed -i 's/fill="black"/fill="white"/g' ../globalassets/avlogo.svg && sed -i 's/$bgc: #fff;/$bgc: #000;/' ./eww.scss; sed -i 's/$fgc: #000;/$fgc: #fff;/' ./eww.scss
+    then
+    sed -i 's/fill="#1c1c1d"/fill="#ffffff"/g' ../globalassets/avlogo.svg
+    sed -i 's/$bgc: #fff;/$bgc: #1c1c1d;/' ./eww.scss
+    sed -i 's/$fgc: #1c1c1d;/$fgc: #fff;/' ./eww.scss
+    sed -i 's/$sbg: #f3f4f9;/$sbg: #2c2c2d;/' ./eww.scss
+    sed -i 's/$lacct;/$dacct;/g' ./eww.scss
 
-elif [[ `cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name | cut -d "=" -f 2` == "Cutefish-light" && `grep '$bgc: #000;' ./eww.scss` == '$bgc: #000;' ]]
-    then sed -i 's/fill="white"/fill="black"/g' ../globalassets/avlogo.svg && sed -i 's/$bgc: #000;/$bgc: #fff;/' ./eww.scss; sed -i 's/$fgc: #fff;/$fgc: #000;/' ./eww.scss
+elif [[ `cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name | cut -d "=" -f 2` == "Cutefish-light" && `grep '$bgc: #1c1c1d;' ./eww.scss` == '$bgc: #1c1c1d;' ]]
+    then
+    sed -i 's/fill="#ffffff"/fill="#1c1c1d"/g' ../globalassets/avlogo.svg
+    sed -i 's/$bgc: #1c1c1d;/$bgc: #fff;/' ./eww.scss
+    sed -i 's/$fgc: #fff;/$fgc: #1c1c1d;/' ./eww.scss
+    sed -i 's/$sbg: #2c2c2d;/$sbg: #f3f4f9;/' ./eww.scss
+    sed -i 's/$dacct;/$lacct;/g' ./eww.scss
 fi
